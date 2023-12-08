78107866321b568effd000d7178d55a0969ed15f
Aditya Jhunjhunwala <109202470+ElectricPlant19@users.noreply.github.com>
2023-12-08 13:45:34 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,27 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
 
+  if (str1.length != str2.length) {
+    return false;
+  }
+
+  let strLength = str1.length;
+  let sum1 = 0;
+  let sum2 = 0;
+
+  for (let i = 0; i < strLength; i++) {
+    sum1 = sum1 + str1.charCodeAt(i);
+    sum2 = sum2 + str2.charCodeAt(i);
+  }
+
+  if (sum1 == sum2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = isAnagram;
