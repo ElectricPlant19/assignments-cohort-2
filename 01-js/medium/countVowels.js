958d2ad7940379f3db11910ab039ae3922482355
Aditya Jhunjhunwala <109202470+ElectricPlant19@users.noreply.github.com>
2023-12-08 13:50:26 +0530

    Update countVowels.js

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -1,4 +1,3 @@
-/*
   Implement a function `countVowels` that takes a string as an argument and returns the number of vowels in the string.
   Note: Consider both uppercase and lowercase vowels ('a', 'e', 'i', 'o', 'u').
 
@@ -6,7 +5,19 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  const lowerStr = str.toLowerCase();
+
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  const vowelCount = Array.from(lowerStr).filter(char => vowels.includes(char)).length;
+
+  return vowelCount;
 }
+
+const testString = "Hello, World!";
+const result = countVowels(testString);
+
+console.log(`The number of vowels in "${testString}" is: ${result}`);
+
 
 module.exports = countVowels;
