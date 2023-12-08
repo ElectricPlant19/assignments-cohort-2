48688319408a43eb6c3cdf27dbd44af968d07102
Aditya Jhunjhunwala <109202470+ElectricPlant19@users.noreply.github.com>
2023-12-08 13:46:28 +0530

    Update times.js

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,15 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+  const before = new Date();
+  const beforeTime = before.getTime();
+
+  let a = 0;
+  for (let i = 0; i < 1000000000; i++) {
+    a += i;
+  }
+
+  const after = new Date();
+  const afterTime = after.getTime();
+  return afterTime - beforeTime;
 }
